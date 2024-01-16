0bd0d9368c83cac7bbd17261d411b2510bbe0c34
Psychon <49412250+psychon-night@users.noreply.github.com>
2024-01-16 13:00:58 -0800

    Update sync_battery.sh

--- sync_battery.sh
+++ sync_battery.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
-
+#!/bin/bash                                
 CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
 AC_STAT=$(cat /sys/class/power_supply/BAT0/status | grep -c Charging)
 
 adb shell dumpsys battery set level $CHARGE
+
+if [ "$AC_STAT" -eq "1" ]; then
+        adb shell dumpsys battery set status 2
+else
+        adb shell dumpsys battery set status 3
+fi
+
 adb shell dumpsys battery set ac $AC_STAT
