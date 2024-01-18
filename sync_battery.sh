6cddb7402090c59f1dca1c9b9cf07d85063797f2
MurderAxo <49412250+psychon-night@users.noreply.github.com>
2024-01-18 11:46:59 -0800

    Update sync_battery.sh

--- sync_battery.sh
+++ sync_battery.sh
@@ -1,13 +1,16 @@
 #!/bin/bash                                
+DEVICE_NAME="emulator-5554" # Enter your ADB device's name here. You can get it with "adb devices"
+
+# Do not modify beyond this point
 CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
 AC_STAT=$(cat /sys/class/power_supply/BAT0/status | grep -c Charging)
 
-adb shell dumpsys battery set level $CHARGE
+adb -s $DEVICE_NAME shell dumpsys battery set level $CHARGE
 
 if [ "$AC_STAT" -eq "1" ]; then
-        adb shell dumpsys battery set status 2
+        adb -s $DEVICE_NAME shell dumpsys battery set status 2
 else
-        adb shell dumpsys battery set status 3
+        adb -s $DEVICE_NAME shell dumpsys battery set status 3
 fi
 
-adb shell dumpsys battery set ac $AC_STAT
+adb -s $DEVICE_NAME shell dumpsys battery set ac $AC_STAT
